daf73663e31857fb783c3a041ea05a580bfe878a
Siva <sbusayavalasa@gmail.com>
2024-04-26 14:45:32 -0700

    initial

--- migration.js
+++ migration.js
@@ -1,5 +1,6 @@
 module.exports = function (migration) {
     // Create a new category field in the blog post content type.
+    console.log("im here");
     const contentType = migration.editContentType('product');
     contentType.createField('test')
       .name('Test')
